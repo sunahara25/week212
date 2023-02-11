0804e176fd864e2014c0c361ac908eedad8e66e2
【sunahara5199】 <sunahara5199@gmail.com>
2023-02-12 02:30:32 +0900

    commit

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'users/show'
+  get 'home/index'
+  get 'users/session'
+  get 'users/passwords'
+  get 'users/registrations'
+  get 'user/id'
+  get 'user/new'
+  
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+
+  resources :users, only: :show
+  
+  resources :users, only: [:show]
+  resources :posts, only: [:create]
+  resources :users, only: [:new]
+  root to: 'home#index'
+  resources :users, only: [:show]
+  
+end
+
+
